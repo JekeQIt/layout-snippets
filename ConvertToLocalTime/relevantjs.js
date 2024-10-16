5769bca62852d86582310447ce98e2d74b2f65fe
jeffkealy <jeffkealy@yahoo.com>
2024-10-16 09:10:14 -0500

    updated converttolocal time

--- ConvertToLocalTime/relevantjs.js
+++ ConvertToLocalTime/relevantjs.js
@@ -1,24 +1,18 @@
 queueViewModel.pageReady(function (data) {
   var pageid = $("body").attr("data-pageid");
+  var culture = $("body").attr("data-culture");
 
   if (pageid == "before") {
   }
 
   if (pageid == "queue") {
-    //Create new elements for expectedServiceTime. overwrite issues can cause the original elements to momentarily display inaccurate times.
-    if ($(".expectedServiceTime").length === 0) {
-      $(`<span class="expectedServiceTime"></span>`).insertAfter("#MainPart_lbExpectedServiceTimeText");
+    //Create new elements for expectedServiceTime. overwrite issues can cause the original elements to momentarsily display inaccurate times.
+    if ($("#expectedServiceTime").length === 0) {
+      $(`<span id="expectedServiceTime" data-bind="visible: layout.expectedServiceTimeVisible"></span>`).insertAfter("#MainPart_lbExpectedServiceTimeText");
     }
-    // if queue is paused/unpaused show/hide expectedservcietime
-    if (queueViewModel.layout.queueIsPausedVisible()) {
-      $(".expectedServiceTime").hide();
-    } else {
-      $(".expectedServiceTime").show();
-    }
-    //Hide expectedServiceTime if serviced soon message is present
-    if (queueViewModel.layout.servicedSoonVisible() === true) {
-      $(".expectedServiceTime").hide();
-    }
+    //apply the Knockout data-bind that's added to the to the new expected service time element.
+    ko.applyBindings(queueViewModel, document.getElementById("expectedServiceTime"));
+
     //Get ISO service time
     const isoTimeString = queueViewModel.options.inqueueInfo.ticket.expectedServiceTimeUTC;
 
@@ -43,19 +37,8 @@ queueViewModel.pageReady(function (data) {
 queueViewModel.modelUpdated(function (data) {
   //Continue updating expectedServiceTime when model waiting room updates.
   const isoTimeString = data.ticket.expectedServiceTimeUTC;
+  //run conversion function
   convertISOTimeToLocalTime(isoTimeString);
-
-  //Hide/show  expectedServiceTime if queue is paused/unpaused
-  if (data.ticket.queuePaused === true) {
-    $(".expectedServiceTime").hide();
-  } else {
-    $(".expectedServiceTime").show();
-  }
-
-  //Hide expectedServiceTime if serviced soon message is present
-  if (data.layout.servicedSoonVisible === true) {
-    $(".expectedServiceTime").hide();
-  }
 });
 
 function convertISOTimeToLocalTime(isoTimeString) {
@@ -78,6 +61,6 @@ function convertISOTimeToLocalTime(isoTimeString) {
     .pop(); // Extract the time zone abbreviation from the string
 
   //update new expectedServiceTime element
-  $(".expectedServiceTime").text(localTime.replace(/\s/g, ""));
+  $("#expectedServiceTime").text(localTime.replace(/\s/g, ""));
   return { localTime, timeZone };
 }
